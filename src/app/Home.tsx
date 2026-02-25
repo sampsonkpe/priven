892a68b258424797858a6fa466d12fd1f2a7d7eb
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2026-02-25 15:14:11 +0000

    Fix missing footer overlay

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -35,7 +35,7 @@ const Home = () => {
             animate={{ opacity: 1 }}
             transition={{
               duration: 1,
-              ease: [0.25, 0.1, 0.25, 1],
+              ease: [0.25, 0.1, 0.256, 1],
               delay: 0.1,
             }}
             className="relative z-10"
